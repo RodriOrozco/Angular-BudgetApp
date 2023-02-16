046f91061f7a1ed7e6f701fe2df42026bc552c08
Rodrigo Orozco <orozcorodri8@gmail.com>
2023-02-16 20:53:34 -0300

    components added and template html ready without styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { IncomesComponent } from './incomes/incomes.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IncomesComponent,
+    ExpensesComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
